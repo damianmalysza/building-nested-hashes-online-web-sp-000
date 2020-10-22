9b70e4c5b14dd63e8656634ad348d1914c630c2e
Damian Malysza <damian.malysza@gmail.com>
2020-10-22 21:58:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -8,7 +8,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        name: "lady Montague",
+        name: "Lady Montague",
         age: "54"
         },
       :hero => {
